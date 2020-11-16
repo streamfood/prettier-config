433309d1f017315b157602dbf4bd3e4ccb9dea6e
itspngu <34752364+itspngu@users.noreply.github.com>
2020-11-16 12:07:06 +0100

    init

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 128,
+    singleQuote: false,
+    trailingComma: "es5",
+    semi: true,
+    quoteProps: "consistent",
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: "always",
+    endOfLine: "lf",
+    embeddedLanguageFormatting: "off"
+};
